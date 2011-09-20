f706f777f6953c7e22cc3fd1efbfd2836a217c1e
sandeep-a-rao <sandeep.a.rao@gmail.com>
2011-09-20 00:11:48 -0400

    Version 1.0

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Project1::Application.config.secret_token = '63649d5a23e2c2eae116045e8bd4bbd51cfbde5d7fc326aa7c408abea156f4926dad72f3e54c24f07116b6892fce7519a26099417db34f76593e28326ea33fb2'
---
+++ db/migrate/20110920021845_create_users.rb
@@ -0,0 +1,13 @@
+class CreateUsers < ActiveRecord::Migration
+  def change
+    create_table :users do |t|
+      t.string :name
+      t.string :email
+
+      t.timestamps
+    end
+  end
+   def delete
+    drop_table :users
+  end
+end
