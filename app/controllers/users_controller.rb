55a37632faa4e6215ed1039c39b4ac0e485283f3
sandeep-a-rao <sandeep.a.rao@gmail.com>
2011-09-20 02:03:05 -0400

    Version 1.2-Stable

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,4 +6,19 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     @title = @user.name
   end
+  def new
+    @user = User.new
+    @title = "Sign up"
+  end
+  def create
+    @user = User.new(params[:user])
+    if @user.save
+       flash[:success] = "Welcome to the Reality!"
+      redirect_to @user
+      # Handle a successful save.
+    else
+      @title = "Sign up"
+      render 'new'
+    end
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Project1::Application.routes.draw do
+  get "sessions/new"
+
+  resources :users
+  resources :sessions, :only => [:new, :create, :destroy]
+
+  match '/signup',  :to => 'users#new'
+  match '/signin',  :to => 'sessions#new'
+  match '/signout', :to => 'sessions#destroy'
+
   get "pages/home"
 
   get "users/new"
